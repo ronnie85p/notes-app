da86aa39ee66fb5a684dd2cc303f2ffbdea599d0
romulus <romagifted@gmail.com>
2024-06-02 19:16:03 +0700

    fixed

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,13 @@ use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\NotesController;
 
 Route::prefix('/v1')->group(function () {
-    Route::post('/auth/login', LoginController::class);
-    Route::get('/auth/logout', [LoginController::class, 'logout']);
-    Route::post('/auth/register', RegisterController::class);
+
+    Route::prefix('/auth')->group(function() {
+        Route::post('/login', LoginController::class);
+        Route::get('/logout', [LoginController::class, 'logout']);
+        Route::post('/register', RegisterController::class);
+    });
     
     Route::apiResource('/notes', NotesController::class);
+    
 });
