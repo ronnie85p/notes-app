80aac7a41ad6e07f06b2dc300b86c5a3e22a755b
romulus <romagifted@gmail.com>
2024-06-06 09:43:21 +0700

    add button create note to profile

--- resources/views/includes/profile/menu.blade.php
+++ resources/views/includes/profile/menu.blade.php
@@ -3,6 +3,11 @@
         <a href="{{ route('profile.show') }}" class="{{ $active == 'profile.show' ? 'fw-bolder' : '' }}">Профиль</a>
         <a href="{{ route('profile.edit') }}" class="float-end text-muted"><small>ред.</small></a>
     </li>
+
+    <li class="list-group-item">
+        <a href="{{ route('home') }}" class="{{ $active == 'home' ? 'fw-bolder' : '' }}">Мои заметки</a>
+    </li>
+
     <li class="list-group-item">
         <a href="{{ route('profile.editpassword') }}" class="{{ $active == 'profile.editpassword' ? 'fw-bolder' : '' }}">Изменить пароль</a>
     </li>
